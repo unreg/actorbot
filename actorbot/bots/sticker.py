1204dc15a7cd981c32f76473d8b0ba2ea2518375
tmnhy <engine@tmnhy.su>
2016-10-16 11:27:14 +0300

    Add check admin grants

--- actorbot/bots/sticker.py
+++ actorbot/bots/sticker.py
@@ -12,13 +12,21 @@ class StickerConv(Conversation):
         ('delsticker', 'delete sticker from current pack'),
         ('showstickers', 'show stickers from current pack'),
         ('help', 'list commands'))
+    admins = ['']
 
     def message_handler(self, message):
         """
         """
-        super().message_handler(message)
-
         peer = message.body.peer
         message = message.body.message
+
         if message.type != 'Text':
+            return
+        if message.text == '/help':
+            self.help(peer)
+        elif message.text == '/start':
+            self.start(peer)
+        elif self.admins and (peer.id not in self.admins):
+            self.sendText(
+                peer, text='Access denied. Administrator rights are required.')
             return
